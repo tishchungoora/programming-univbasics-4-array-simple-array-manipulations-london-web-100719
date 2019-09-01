1c71f98a95316bdbd27f02e6282f8de49d965f83
Tish Chungoora <tishal82@gmail.com>
2019-09-01 13:10:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, element)
   array.delete(element)
 end
 
-def using_delete_at(array, int)
-  array.delete_at(int)
+def using_delete_at(array, index_number)
+  array.delete_at(index_number)
 end
