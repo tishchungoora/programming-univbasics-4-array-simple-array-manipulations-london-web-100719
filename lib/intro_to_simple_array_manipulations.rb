3eb1d54701876853195ef93b6559b0ffd3c5ff5a
Tish Chungoora <tishal82@gmail.com>
2019-09-01 12:45:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,21 @@
 def using_push(array, element)
   array.push(element)
 end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args()
+  
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args()
